44c64067223bad56270e9405052b7815c66232c0
Carlos Rene Salgado Baez <carlos.salgado85@uabc.edu.mx>
2025-12-01 21:51:28 -0800

    arreglo menor con doble inicializacion uvicorn

--- main.py
+++ main.py
@@ -220,13 +220,3 @@ async def root():
             "GET /docs": "Documentacion interactiva (Swagger)"
         }
     }
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-        log_level="info"
-    )
